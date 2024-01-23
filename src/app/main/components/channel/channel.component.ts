c61284116098058bbb94d43e52bee1b3c5711472
ptakf <s222693@student.uek.krakow.pl>
2024-01-23 16:47:36 +0100

    Add basic channel view

--- src/app/main/components/channel/channel.component.ts
+++ src/app/main/components/channel/channel.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Channel } from '../../models/channel';
+import { ChannelService } from '../../services/channel.service';
 
 @Component({
   selector: 'app-channel',
@@ -6,4 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss',
 })
-export class ChannelComponent {}
+export class ChannelComponent implements OnInit {
+  channel: Channel = new Channel();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private channelService: ChannelService,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.channel = this.channelService.getChannel(id as string);
+  }
+
+  onRemovedChannel(channel: Channel): void {
+    if (confirm('Are you sure you want to remove this channel?')) {
+      this.channelService.removeChannel(channel);
+
+      this.router.navigate(['/channel/list']);
+    }
+  }
+}
